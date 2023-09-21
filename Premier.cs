9f7716478eeb1d9ddb966da3560653b725fcb03b
Toinoup80 <antoine.psn80@gmail.com>
2023-09-21 10:58:35 +0200

    Code début crible eratosthene

--- Premier.cs
+++ Premier.cs
@@ -3,9 +3,15 @@
 public static class Premier
 {
 
-	public static CribleEratosthene()
-	{
 
+	public static CribleEratosthene(int n)
+	{
+		int[] listeEntiers = new int[n];
+		for (int i = 1; i <= n; i++)
+		{
+			listeEntiers[i] = i;
+			Console.WriteLine(i);
+		}
 	}
 
 	public static nbPremierEntreEux()
