12dea59b4d8108b7d0032abb2d7cdaa47ff91d48
Antto <coutreelantoine@gmail.com>
2023-09-21 11:39:58 +0200

    Modification de la méthode nbPremierEntreEux

--- Premier.cs
+++ Premier.cs
@@ -22,9 +22,85 @@ public static class Premier
          * 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97.*/
     }
 
-	public static nbPremierEntreEux()
+	public static bool nbPremierEntreEux(int a, int b)
 	{
+		int PGCD = 0;
+		int r;
 
+		if (a > b && b != 0)
+		{
+            for{
+				r = a % b;
+
+                if (r == 0)
+                {
+                    PGCD = b;
+                }
+                else
+                {
+                    a = b;
+                    b = r;
+                }
+            } while (r != 0) ;
+
+			if(PGCD == 1)
+			{
+                Console.Writeline($"{a} et {b} sont premiers entre eux");
+			}
+
+			if(b == 1)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+        }
+		else
+		{
+			if(b < a)
+			{
+                int echange = a;
+                a = b;
+                b = echange;
+
+                Console.Writeline($"Les valeurs {a} et {b} ont ete echanges pour trouver le PGCD.");
+
+                for{
+                    r = a % b;
+
+                    if (r == 0)
+                    {
+                        PGCD = b;
+                    }
+                    else
+                    {
+                        a = b;
+                        b = r;
+                    }
+                } while (r != 0) ;
+
+                if (PGCD == 1)
+                {
+                    Console.Writeline($"{a} et {b} sont premiers entre eux");
+                }
+
+                if (b == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+			else
+			{
+				return -1;
+			}
+		}
+		
 	}
 
 	public static DFP() // Décomposition en produit de Facteurs Premiers
