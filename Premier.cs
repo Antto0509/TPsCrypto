b79c8d17803dccd9646c5c1e060fb30bf5fd37ea
Toinoup80 <antoine.psn80@gmail.com>
2023-09-21 15:47:23 +0200

    Nouvelle version crible

--- Premier.cs
+++ Premier.cs
@@ -8,33 +8,38 @@ public static class Premier
 
 	public static void CribleEratosthene(int n)
 	{
-		int nbDiviseurs;
+        int nbDiviseurs;
 
-		int[] listeEntiers = new int[n];
-		bool[] listeBiffe = new bool[n];
-		for (int i = 1; i <= n; i++)
-		{
-			listeEntiers[i-1] = i;
-			Console.WriteLine(i);
-			nbDiviseurs = 0;
+        int[] listeEntiers = new int[n];
+        bool[] listeBiffe = new bool[n];
 
-            for (int j = 1; j <= Math.Sqrt(n); j++)
+        listeBiffe[0] = false;
+
+        for (int i = 1; i <= n; i++)
+        {
+            listeEntiers[i - 1] = i;
+            Console.WriteLine(i);
+            nbDiviseurs = 0;
+            for (int j = 1; j <= n; j++)
             {
-				if((i%j == 0) && (i!= 1))
-				{
-					nbDiviseurs++;
-				}
+                if ((i % j == 0) && (j <= i) && (i != 1))
+                {
+                    nbDiviseurs++;
+                }
             }
-			
-			if(nbDiviseurs <= 2)
-			{
-				listeBiffe[i-1] = true;
-			}
-			else
-			{
-				listeBiffe[i - 1] = false;
-			}
 
+            if (nbDiviseurs == 2)
+            {
+                listeBiffe[i - 1] = true;
+            }
+            else
+            {
+                listeBiffe[i - 1] = false;
+            }
+        }
+        for (int k = 0; k < n; k++)
+        {
+            Console.WriteLine(listeBiffe[k]);
         }
 
         /*Jeux de test.Les nombres premiers inférieurs à 100 sont: 
