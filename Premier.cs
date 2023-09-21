a4967ff4f1c0c6d62c6ef2b765ed5da1ddc1cd59
Antto <coutreelantoine@gmail.com>
2023-09-21 11:00:26 +0200

    Ajout d'un commentaire

--- Premier.cs
+++ Premier.cs
@@ -15,7 +15,12 @@ public static class Premier
 			listeEntiers[i] = i;
 			Console.WriteLine(i);
 		}
-	}
+
+
+
+        /*Jeux de test.Les nombres premiers inférieurs à 100 sont: 
+         * 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97.*/
+    }
 
 	public static nbPremierEntreEux()
 	{
