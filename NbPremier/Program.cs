a92f4d19cd3a53c2e81138c82bbfd8dff9bc8be9
Toinoup80 <antoine.psn80@gmail.com>
2023-10-05 13:51:42 +0200

    Affichage fonctions ajouté

--- NbPremier/Program.cs
+++ NbPremier/Program.cs
@@ -10,10 +10,15 @@ namespace NbPremier
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\nCRIBLE ERATOSTHENE :\n");
             Console.WriteLine(Premier.CribleEratosthene(150));
+            Console.WriteLine("\nNOMBRE PREMIERS ENTRE EUX :\n");
             Console.WriteLine(Premier.nbPremierEntreEux(2, 6));
+            Console.WriteLine("\nDFP :\n");
             Console.WriteLine(Premier.DFP(50));
+            Console.WriteLine("\nINDICATEUR EULER :\n");
             Console.WriteLine(Premier.IndicateurEuler(50));
+            Console.WriteLine("\nPUISSANCE MODULO :\n");
             Console.WriteLine(Premier.PuissanceModulo(4, 13, 497));
         }
 			
