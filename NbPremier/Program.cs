9510cc91a2b333578e1a63340817b1ade63c6b7e
Toinoup80 <127845250+Toinoup80@users.noreply.github.com>
2023-11-23 15:01:41 +0100

    Modifications

--- NbPremier/Program.cs
+++ NbPremier/Program.cs
@@ -10,6 +10,7 @@ namespace NbPremier
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("\nCRIBLE ERATOSTHENE :\n");
             Console.WriteLine(Premier.CribleEratosthene(150));
             Console.WriteLine("\nNOMBRE PREMIERS ENTRE EUX :\n");
@@ -20,7 +21,50 @@ namespace NbPremier
             Console.WriteLine(Premier.IndicateurEuler(50));
             Console.WriteLine("\nPUISSANCE MODULO :\n");
             Console.WriteLine(Premier.PuissanceModulo(4, 13, 497));
-        }
-			
+            */
+            Console.WriteLine("\nCRIBLE ERATOSTHENE :\n");
+            bool[] cribleResult = Premier.CribleEratosthene(150);
+            for (int i = 2; i < cribleResult.Length; i++)
+            {
+                if (cribleResult[i])
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine("\n\nNOMBRE PREMIERS ENTRE EUX :\n");
+            Console.WriteLine(Premier.nbPremierEntreEux(2, 6));
+
+            Console.WriteLine("\n\nDFP :\n");
+            var dfpResult = Premier.DFP(50);
+            for (int i = 0; i < dfpResult.Item1.Count; i++)
+            {
+                Console.WriteLine($"{dfpResult.Item1[i]}^{dfpResult.Item2[i]}");
+            }
+
+            Console.WriteLine("\n\nINDICATEUR EULER :\n");
+            Console.WriteLine(Premier.IndicateurEuler(50));
+
+            Console.WriteLine("\n\nPUISSANCE MODULO :\n");
+            Console.WriteLine(Premier.PuissanceModulo(4, 13, 497));
+
+            // Test de la classe RSA
+            Console.WriteLine("\nTEST DE LA CLASSE RSA :\n");
+
+            // Jeux de test
+            int p1 = 47, q1 = 71, e1 = 79;
+            int p2 = 5, q2 = 17, e2 = 5;
+            int p3 = 4, q3 = 11, e3 = 3; // Clé invalide
+            int p4 = 3, q4 = 11, e4 = 40; // Clé invalide
+
+            RSA rsa1 = new RSA(p1, q1, e1);
+            rsa1.AfficherCleDechiffrement();
+
+            RSA rsa2 = new RSA(p2, q2, e2);
+            rsa2.AfficherCleDechiffrement();
+
+            RSA rsa3 = new RSA(p3, q3, e3); // Ne devrait pas être créé
+            RSA rsa4 = new RSA(p4, q4, e4); // Ne devrait pas être créé
+        }	
     }
 }
--- NbPremier/RSA.cs
+++ NbPremier/RSA.cs
@@ -8,6 +8,79 @@ namespace NbPremier
 {
     internal class RSA
     {
+        private int p;
+        private int q;
+        private int e;
+        private int n;
+        private int d;
 
+        // Constructeur
+        public RSA(int p, int q, int e)
+        {
+            if (EstValid(p, q, e))
+            {
+                this.p = p;
+                this.q = q;
+                this.e = e;
+                this.n = p * q;
+                this.d = CalculerD();
+            }
+            else
+            {
+                Console.WriteLine("Clé de chiffrement invalide.");
+            }
+        }
+
+        // Méthode pour vérifier la validité de la clé de chiffrement
+        private bool EstValid(int p, int q, int e)
+        {
+            int phiN = (p - 1) * (q - 1);
+            return Premier.nbPremierEntreEux((int)e, (int)phiN) && p > 1 && q > 1 && e > 1 && n > 1;
+        }
+
+        // Méthode pour calculer la clé de déchiffrement d
+        private int CalculerD()
+        {
+            int phiN = (p - 1) * (q - 1);
+            int d = AlgorithmeEuclidienEtendu(e, phiN);
+            if (d < 0)
+            {
+                d += phiN; // Assurer que d est positif
+            }
+            return d;
+        }
+
+        // Algorithme d'Euclide étendu
+        private int AlgorithmeEuclidienEtendu(int a, int b)
+        {
+            int x0 = 1, x1 = 0, y0 = 0, y1 = 1;
+            while (b != 0)
+            {
+                int q = a / b;
+                int temp = b;
+                b = a % b;
+                a = temp;
+                temp = x0;
+                x0 = x1;
+                x1 = temp - q * x1;
+                temp = y0;
+                y0 = y1;
+                y1 = temp - q * y1;
+            }
+            return x0;
+        }
+
+        // Méthode pour afficher la clé de déchiffrement
+        public void AfficherCleDechiffrement()
+        {
+            if (d != 0)
+            {
+                Console.WriteLine($"Clé de déchiffrement : {d}");
+            }
+            else
+            {
+                Console.WriteLine("Clé de déchiffrement non calculée.");
+            }
+        }
     }
 }
