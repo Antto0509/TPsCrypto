3a348d41515f097281c7d0d989c354ca929e20cb
Toinoup80 <127845250+Toinoup80@users.noreply.github.com>
2023-11-23 15:54:25 +0100

    Update Program.cs

--- NbPremier/Program.cs
+++ NbPremier/Program.cs
@@ -36,21 +36,21 @@ namespace NbPremier
             Console.WriteLine(Premier.nbPremierEntreEux(2, 6));
 
             Console.WriteLine("\n\nDFP :\n");
-            var dfpResult = Premier.DFP(50);
+            var dfpResult = Premier.DFP(1500);
             for (int i = 0; i < dfpResult.Item1.Count; i++)
             {
                 Console.WriteLine($"{dfpResult.Item1[i]}^{dfpResult.Item2[i]}");
             }
 
             Console.WriteLine("\n\nINDICATEUR EULER :\n");
-            Console.WriteLine(Premier.IndicateurEuler(50));
+            Console.WriteLine(Premier.IndicateurEuler(10403));
 
             Console.WriteLine("\n\nPUISSANCE MODULO :\n");
             Console.WriteLine(Premier.PuissanceModulo(4, 13, 497));
 
             // Test de la classe RSA
             Console.WriteLine("\nTEST DE LA CLASSE RSA :\n");
-            /*
+
             // Jeux de test
             int p1 = 47, q1 = 71, e1 = 79;
             int p2 = 5, q2 = 17, e2 = 5;
@@ -65,7 +65,7 @@ namespace NbPremier
 
             RSA rsa3 = new RSA(p3, q3, e3); // Ne devrait pas être créé
             RSA rsa4 = new RSA(p4, q4, e4); // Ne devrait pas être créé
-            */
+
         }	
     }
 }
